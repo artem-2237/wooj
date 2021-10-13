bd02d8f9cee7e8f6c1f4cbe6daeddc367aee7fe0
artem-2237 <valera8461@gmail.com>
2021-10-13 04:47:54 +0300

    created main slider, changed media breakpoints, minor edits in footer

---
+++ js/sliders.js
@@ -0,0 +1,7 @@
+$('#main-slider').slick({
+    infinite: true,
+    nextArrow: $('.main-slider__arrow-right'),
+    prevArrow: $('.main-slider__arrow-left'),
+    dots: true,
+    dotsClass: 'main-slider__dots'
+});
